6cbf30e3441bcbd8c0605c4356d25e999fb6ea16
gelpi <gelpi@ub.edu>
2025-03-23 12:43:43 +0100

    Updated to FEM, tested partially

--- demo/vre_template_tool/dt4h_demonstrator.py
+++ demo/vre_template_tool/dt4h_demonstrator.py
@@ -1,6 +1,6 @@
 # OPENVRE EXECUTOR FOR FEMMANAGER (FORMER FLMANAGER)
 # ============================================================================
-# Executor for the DT4H flcore demonstrators folling the demo notebook
+# Executor for the DT4H flcore demonstrators filling the demo notebook
 
 import sys
 import time
@@ -14,10 +14,11 @@ PASSWORD = 'demo'
 KEYCLOAK_URL = 'https://inb.bsc.es/auth/realms/datatools4heart/protocol/openid-connect/token'
 API_PREFIX = 'https://fl.bsc.es/dt4h_fem/API/v1'
 
-def _create_header( token ):
+
+def _create_header(token: str) -> dict:
     return { 
         'Authorization': f'Bearer {token}',
-        'Accept':'application/json'
+        'Accept': 'application/json'
     }
 
 
@@ -45,19 +46,22 @@ def get_fedmanager_token(user: str, pwd: str) -> str:
     else:
         raise Exception(f"Failed to obtain token: {response.status_code}, {response.text}")
 
+
 # NODES HEALTH CHECK
 # ----------------------------------------------------------------------------
-def nodes_health_check(api_prefix, access_token: str, nodes: list[str]) -> dict:
+def node_heartbeat(api_prefix, access_token: str, node: str) -> dict:
     headers = _create_header(access_token)
-    health_data = {}
-    for node in nodes:
-        url = f"{api_prefix}/hosts/health?nodes={node}"
-        response_data = requests.get(url, headers=headers)
-        if response_data.status_code != 200:     
-            health_data[node] = (response_data.status_code, response_data)
-        else:
-            health_data[node] = response_data.json()
-    return health_data
+    heartbeat_data = {}
+    url = f"{api_prefix}/hosts/heartbeat?node_name={node}"
+    
+    response_data = requests.get(url, headers=headers)
+    
+    if response_data.status_code != 200:   
+        heartbeat_data[node] = (response_data.status_code, response_data)
+    else:
+        heartbeat_data[node] = response_data.json()
+    
+    return heartbeat_data
 
 
 # EXECUTE TOOL IN NODES
@@ -71,22 +75,29 @@ def execute_tool(
         params: dict = None
     ) -> dict:
     logging.info(
-        f"Triggering tool {tool_name} in nodes {','.join(server_nodes + client_nodes)}"
+        f"Triggering tool {tool_name} in server nodes {server_node} and {','.join(client_nodes)} client nodes"
     )
 
-    # TODO use nodes split on server, client. Default use DB
     headers = _create_header(access_token)
-    url = f"{api_prefix}/tools/job/{tool_name}"
+
+    url = f"{api_prefix}/tools/job/{tool_name}/"
+    url_params = []
+
     if client_nodes is not None:
-        url += f"?client_nodes={ ','.join(client_nodes)}
+        for node in client_nodes:
+            url_params.append(f"client_nodes={node}")
+
     if server_node is not None:
-        url += f"&server_node={server_node}"
+        url_params.append(f"server_node={server_node}")   
+
+    if url_params:
+        url += f'?{"&".join(url_params)}'
 
     logging.debug("\t-- FedManager URL = {}".format(url))
-    
+
     params_data = json.dumps(params) if params is not None else "{}"
 
-    response_data = requests.post(url, headers = headers, data=params_data)
+    response_data = requests.post(url, headers=headers, data=params_data)
     response_data.raise_for_status()
 
     return response_data.json()
@@ -94,13 +105,22 @@ def execute_tool(
 
 # CHECK STATUS OF TOOL IN NODES
 # ----------------------------------------------------------------------------
-def inquiry_tool(api_prefix, access_token:str, execution:str ) -> dict:
-    headers = _create_header(access_token )
+def inquiry_execution_status(api_prefix, access_token: str, execution_id: str) -> dict:
+    headers = _create_header(access_token)
 
-    url = f'{ api_prefix }/tools/jobs/{execution}/status'
-    response_data = requests.get ( url, headers = headers )
+    url = f'{ api_prefix }/executions/status/{execution_id}'
+    response_data = requests.get(url, headers=headers)
 
     return response_data.json()
+
+
+def check_job_finished(job_status: dict) -> bool:
+    job_status = {}
+    for node_status in job_status:
+        node = node_status['node']
+        if job_status[node]['result'] == 'running':
+            return False
+    return True
 
 
 # FULL PIPELINE
@@ -121,38 +141,41 @@ def dt4h_demonstrator(
         logging.info("Getting token from Keycloak")
         access_token = get_fedmanager_token('demo@bsc.es', 'demo')
 
+    logging.info("Checking nodes health")
+    server_active_node = None
     if health_check:
         logging.info("Checking server health")
-        server_active_node = []
-        health_check_data = nodes_health_check(
+        server_active_node = None
+        health_check_data = node_heartbeat(
             api_prefix,
             access_token,
             server_node
         )
-        logging.info(f"servers: {health_check_data}")
-        for node, data in health_check_data.items():
-            if data['status'] == 'success':  # TODO check health status
-                server_active_node.append(node)
+        logging.info(f"server: {health_check_data}")
+
+        if 'state' in health_check_data[0] and health_check_data[0]['state'] == 'running':
+            server_active_node = server_node
+
     else:
         server_active_node = server_node
 
     client_active_nodes = []
     if health_check:
         logging.info("Checking client nodes health")
-        health_check_data = nodes_health_check(
-            api_prefix,
-            access_token,
-            client_node_list
-        )
-        logging.info(f"clients: {health_check_data}")
-        for node, data in health_check_data.items():
-            if data['status'] == 'success':  # TODO check health status
+        for node in client_node_list:
+            health_check_data = node_heartbeat(
+                api_prefix,
+                access_token,
+                node
+            )
+            logging.info(f"clients: {health_check_data}")
+            if 'state' in health_check_data[0] and health_check_data[0]['state'] == 'running':
                 client_active_nodes.append(node)
     else:
-        client_active_nodes = client_node_list    
+        client_active_nodes = client_node_list
 
-    if len(server_active_nodes) == 0 or len(client_active_nodes) == 0:
-        return {'status': 'failure', 'message': 'No active nodes found.'}
+    if not server_active_node or len(client_active_nodes) == 0:
+        return {'status': 'failure', 'message': 'No enough active nodes found.'}
 
     logging.info("Running tool on nodes")
 
@@ -161,30 +184,26 @@ def dt4h_demonstrator(
         access_token,
         server_active_node,
         client_active_nodes,
-        tool_nameid,
+        tool_id,
         params=input_params
     )
     if step_one['status'] != 'success':
         return ({
-            'status': 'failure', 
+            'status': 'failure',
             'message': f"Failed to run \"execute_tool\" on nodes {client_active_nodes} with tool \"{tool_id}\"." 
         })
     else:
-        process_id = step_one['result'][1]
+        execution_id = step_one['result']['execution_id']
 
     # STEP 2 - Do-while until the docker containing the tool dies
     while True:
         wait()
-        step_two = inquiry_tool(api_prefix, access_token, process_id)
-        if step_two['status'] != 'success':
+        job_status = inquiry_execution_status(api_prefix, access_token, execution_id)
+        logging.info(f"Job status: {job_status}")
+        if check_job_finished(job_status):
             break
 
-    # STEP 3 - Return the results
-    if step_two['status'] == 'success':
-        return {'status': 'success', 'message': f"Tool \"{tool_id}\" run on nodes {client_active_nodes}." }
-    else:
-        return { 'status': 'failure', 'message': f"Failed to run \"inquiry_tool\" on nodes {node_list } with tool \"{ tool_name }\" and process \"{ process_id }\"." }
-
+    return {'status': 'success', 'message': f"Tool \"{tool_id}\" run on server {server_active_node} and clients {client_active_nodes}." }
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
