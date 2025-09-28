dc1c6c2946f0f53f45d52675e54d585b11de8015
Josep Ll. Gelpí <gelpi@ub.edu>
2025-09-29 00:33:02 +0200

    Added separated process_parameters

--- dt4h-flcore/dt4h_flcore_tool/flcore_params.py
+++ dt4h-flcore/dt4h_flcore_tool/flcore_params.py
@@ -18,63 +18,35 @@ DEFAULT_TRAIN_LABELS =  [
 
 DEFAULT_TARGET_LABEL = 'conditions_stroke_any'
 
-class FlcoreDataset:
-    ''' Class to represent the FLCore dataset as stored in VRE user space'''
-    def __init__(self, input_dataset_path: str = None):
-        if input_dataset_path is None:
-            self.dataset = {}
-            return
-        with open(input_dataset_path, 'r', encoding='utf-8') as dataset_file:
-            try:
-                self.dataset = json.load(dataset_file)
-            except json.JSONDecodeError:
-                logging.error(f"Failed to load dataset from {input_dataset_path}: {e}")
-                raise ValueError(f"Failed to load dataset file: {e}")
-
-    def get_dataset_id(self):
-        return self.dataset.get('dataset_id', None)
-
-
-class FlcoreParams:
+class FlcoreParams(DT4HToolParams):
     ''' Class to represent the FLCore parameters for model training'''
-    def __init__(self, input_params_path: str = None, num_clients:int = 1, dataset_id: str = None):
-        if input_params_path is None:
-            input_params = {}
-            self.input_params = {
+
+    def process_parameters(self):
+        '''Prepare the parameters for the FLCore tool.'''
+        if self.input_params_path is None:
+            self.final_params = {
                 'server': {
                     'n_features': len(DEFAULT_TRAIN_LABELS),
                     'num_rounds': 1,
-                    'num_clients': num_clients
+                    'num_clients': self.num_clients
                 },
                 'model': 'random_forest',
                 'train_labels': ' '.join(DEFAULT_TRAIN_LABELS),
                 'target_label': DEFAULT_TARGET_LABEL,
-                'data_id': dataset_id
+                'data_id': self.dataset_ids[0] if self.dataset_ids else None
             }
         else:
-            try:
-                with open(input_params_path, 'r', encoding='utf-8') as params_file:
-                    if input_params_path.endswith('.json'):
-                        input_params = json.load(params_file)
-                    elif input_params_path.endswith('.yml') or input_params_path.endswith('.yaml'):
-                        input_params = yaml.safe_load(params_file)
-                    else:
-                        raise ValueError('Unsupported file format. Use JSON or YAML.')
-            except (json.JSONDecodeError, yaml.YAMLError) as e:
-                logging.error(f"Failed to load input parameters from {input_params_path}: {e}")
-                raise ValueError(f"Failed to load input parameters file: {e}")
-
-            self.input_params = {
+            self.final_params = {
                 'server': {
-                    'n_features': len(input_params['train_labels']),
-                    'num_rounds': input_params.get('num_rounds', 1),
-#                    'model': input_params.get('model', 'random_forest'),
-                    'num_clients': num_clients
+                    'n_features': len(self.input_params['train_labels']),
+                    'num_rounds': self.input_params.get('num_rounds', 1),
+#                    'model': self.input_params.get('model', 'random_forest'),
+                    'num_clients': self.num_clients
                 },
-                'model': input_params.get('model', 'random_forest'),
-                'train_labels': ' '.join(input_params.get('train_labels', DEFAULT_TRAIN_LABELS)),
-                'target_label': input_params.get('target_label', DEFAULT_TARGET_LABEL),
-                'data_id': dataset_id
+                'model': self.input_params.get('model', 'random_forest'),
+                'train_labels': ' '.join(self.input_params.get('train_labels', DEFAULT_TRAIN_LABELS)),
+                'target_label': self.input_params.get('target_label', DEFAULT_TARGET_LABEL),
+                'data_id': self.dataset_ids[0] if self.dataset_ids else None
             }
 
     def get_params_json(self):
